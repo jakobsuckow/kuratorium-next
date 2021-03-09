4b691cbdc2d292c3306adc02d06c8cf0f0e1d3b8
Jakob Suckow <jakob@martian.agency>
2021-03-09 22:38:04 +0100

    added types, prettier

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+interface Props {}
+
+const Index: NextPage<Props> = (props: Props) => {
+  const {} = props;
+  return <></>;
+};
+export default Index;
