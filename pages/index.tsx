d1f47fd244db6b7a446fdf8600005f74cba6c839
Jakob Suckow <jakob@martian.agency>
2021-03-09 23:20:09 +0100

    added barebone

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,27 @@ import React from "react";
 
 interface Props {}
 
+const width: number = 479.984375;
+
 const Index: NextPage<Props> = (props: Props) => {
   const {} = props;
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <div className="row">
+        <div className="intro"></div>
+        <div className="about"></div>
+        <div className="projects"></div>
+        <div className="feed"></div>
+      </div>
+
+      <div
+        className="sideshop"
+        style={{
+          width: width,
+          transform: `translateX(${width}px)`,
+          marginRight: `-${width}px`,
+        }}></div>
+    </>
+  );
 };
 export default Index;
