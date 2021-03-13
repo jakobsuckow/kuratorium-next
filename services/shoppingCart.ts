8440f2b727f3b46e088f52908b96b7a5e96bfda5
Jakob Suckow <jakob@martian.agency>
2021-03-13 13:21:18 +0100

    added turnery

--- services/shoppingCart.ts
+++ services/shoppingCart.ts
@@ -1,8 +1,7 @@
 import { CartItem } from "../@types";
 
 export const addToCart = (item: CartItem) => {
-  const currentCart = localStorage.getItem("cart");
-
+  const currentCart = localStorage.getItem("cart") ? localStorage.getItem("cart") : "[]";
   let products: Array<any> = [];
   if (currentCart) {
     products = [JSON.parse(currentCart)];
