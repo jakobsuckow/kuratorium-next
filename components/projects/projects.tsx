38044eede0c1e62256e945353c77d8b3a5a0e3f5
Jakob Suckow <jakob@martian.agency>
2021-03-17 20:39:40 +0100

    deleted console logs

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -38,14 +38,12 @@ const Releases: React.FC<Props> = (props: Props) => {
         read all about it here.
       </p>
       {albums.map((album: Album, i: number) => (
-        <>
-          <div key={i}>
-            <b>{album.name}</b>
-            <p>{album.text}</p>
-            <img src={album.artwork[0].url} />
-          </div>
+        <div key={i}>
+          <b>{album.name}</b>
+          <p>{album.text}</p>
+          <img src={album.artwork[0].url} />
           <br />
-        </>
+        </div>
       ))}
       <h1>Merch</h1>
       {merch.map((merch: Merch, index: number) => (
--- components/shop/shop.tsx
+++ components/shop/shop.tsx
@@ -12,7 +12,6 @@ const Shop: React.FC<Props> = (props: Props) => {
   const [cart, setCart] = React.useState<any | null>([]);
 
   React.useEffect(() => {
-    console.log(`hi`);
     const localItems = localStorage.getItem("cart");
     setCart(JSON.parse(localItems as string));
   }, [setShifted, clearCart, removeFromCart]);
