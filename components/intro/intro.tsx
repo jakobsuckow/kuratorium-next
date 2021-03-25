3d8561a02352f4822acc0d2a569baa34d0a26f5f
Jakob Suckow <jakob@martian.agency>
2021-03-25 15:44:02 +0100

    cleanup

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -1,12 +1,8 @@
 import React, { Dispatch, SetStateAction } from "react";
-import Button from "../button/button";
 import ShoppingBag from "../cart/shoppingBag";
 import Link from "../link/link";
 import Logo from "../logo/logo";
-import Block from "../text/block";
 import Heading from "../text/heading";
-import Text from "../text/text";
-import Underline from "../text/underline";
 import Welcome from "../welcome/welcome";
 
 interface Props {
