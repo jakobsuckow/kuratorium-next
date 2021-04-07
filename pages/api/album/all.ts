1a5ef9a781a75f4dcc129fa11d6df07008d2c239
Jakob Suckow <jakob@martian.agency>
2021-04-07 21:51:53 +0200

    asd

--- pages/api/album/all.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getData } from "../../../services/airtable";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<string>) {
-  const response = await getData("Albums");
-  res.status(200).json(response);
-}
--- pages/api/event/all.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getData } from "../../../services/airtable";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Event>) {
-  const response = await getData("Events");
-  res.status(200).json(response);
-}
-
