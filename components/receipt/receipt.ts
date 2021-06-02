c8720ecbf5e1c929c35622acfa8a7edb59cdfa48
Jakob Suckow <jakob@martian.agency>
2021-06-02 11:32:14 +0200

    comp(receipt): padding for mobile

--- components/receipt/receipt.ts
+++ components/receipt/receipt.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Receipt = styled.div`
   max-width: 40%;
   margin: 40px auto;
-  padding: 0 4px;
+  padding: 0 8px;
 
   @media screen and (max-width: ${props => props.theme.breakpoints.md}) {
     max-width: 100%;
